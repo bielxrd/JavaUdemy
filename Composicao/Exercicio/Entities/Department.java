b8493321a62448a7604381e92c4ec05aa2071514
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-11-03 00:53:07 -0300

    Classe department

---
+++ Composicao/Exercicio/Entities/Department.java
@@ -0,0 +1,20 @@
+package Composicao.Exercicio.Entities;
+
+public class Department {
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
