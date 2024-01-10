131639cebb04dad8947c45c21ee2fc21fca68933
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-10 19:23:54 -0300

    Palavra final em classes e metodos

--- Herança/Entities/SavingAccounts.java
+++ Herança/Entities/SavingAccounts.java
@@ -1,6 +1,6 @@
 package Herança.Entities;
 
-public class SavingAccounts extends Account {
+public final class SavingAccounts extends Account { // final na hora de definir a classe serve para que nao seja possivel ter subclasses de Saving Acoounts
     private Double interestRate;
 
     public SavingAccounts(Integer number, String holder, Double balance, Double interestRate) {
@@ -25,8 +25,16 @@ public class SavingAccounts extends Account {
     }
 
     @Override
-    public void withdraw(double amount) {
+    public final void withdraw(double amount) { // Final no metodo restringe a sobrescrita deste metodo.
         balance -= amount;
     }
+
+    // Vantagens da palavra final na classe ou metodo:
+
+    // - Seguranca
+
+    // - Perfomance: Atributos de tipo de uma classe final sao analisados de forma mais rapido em tempo de execucao
+
+    // Geralmente, convem acrescentar o final em metodos sobrepostos, pois muitas sobreposicoes podem ser uma porta de entrada para inconsistencias
 
 }
